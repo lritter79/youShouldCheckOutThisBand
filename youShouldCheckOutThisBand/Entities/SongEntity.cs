11fc84724953b53e1ad93e8e12a9b33f537042f9
Ritter <lritter@cecinc.com>
2020-04-18 16:31:48 -0400

    added properties to song entity

--- youShouldCheckOutThisBand/Entities/SongEntity.cs
+++ youShouldCheckOutThisBand/Entities/SongEntity.cs
@@ -7,6 +7,34 @@ namespace youShouldCheckOutThisBand.Entities
 {
     public class SongEntity
     {
-        public int Id { get; set; }
+        /// <summary>
+        /// The Spotify ID for the track.
+        /// </summary>
+        public string Id { get; set; }
+
+        //fk for an albums table 
+        public int AlbumId { get; set; }
+
+        //don't include artist ad because a track can have a one to many relationship with artists, so that would require a jointable with song id and artist ids
+
+        /// <summary>
+        /// A link to the Web API endpoint providing full details of the track.
+        /// </summary>
+        public string Href { get; set; }
+
+        /// <summary>
+        /// A link to a 30 second preview (MP3 format) of the track. Can be null
+        /// </summary>
+        public string PreviewUrl { get; set; }
+
+        /// <summary>
+        /// The Spotify URI for the track.
+        /// </summary>
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// The name of the track.
+        /// </summary>
+        public string Name { get; set; }
     }
 }

a51c395fa3cb4bb5ae2a6fc1b30fa7d96ff9d70d
Ritter <lritter@cecinc.com>
2020-04-22 22:02:04 -0400

    made repository class

---
+++ youShouldCheckOutThisBand/Data/IYSCOTBRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using youShouldCheckOutThisBand.Entities;
+
+namespace youShouldCheckOutThisBand.Data
+{
+    public interface IYSCOTBRepository
+    {
+        IEnumerable<ArtistEntity> GetAllArtistEntities();
+        IEnumerable<ArtistEntity> GetArtistsByGenre(string genre);
+    }
+}

bb8443b39e9295a6e57de53f24532da1e0258b1b
Levon Ritter <levong2112@gmail.com>
2020-04-28 00:33:52 -0400

    angular component finally f***ing works

---
+++ youShouldCheckOutThisBand/wwwroot/clientapp/dist/out-tsc/ClientApp/app/app.component.js
@@ -0,0 +1,17 @@
+import { __decorate } from "tslib";
+import { Component } from '@angular/core';
+//change this to include external template, maybe use polymer for this later
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'Suggested Artists';
+    }
+};
+AppComponent = __decorate([
+    Component({
+        selector: 'artists-model',
+        templateUrl: "./app.component.html",
+        styles: []
+    })
+], AppComponent);
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
